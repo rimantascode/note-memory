8b437c68cef2bc4b0e525c445cc2b6193886852a
rimantascode <r.preiks@gmail.com>
2021-08-27 14:13:45 +0000

    it has the model of the product.

---
+++ products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    Description = models.TextField(max_length=1000, blank=False)
+    quantity = models.IntegerField()
+
+
