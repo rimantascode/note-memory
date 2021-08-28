19ab29f251f7d56e11005f6f05bcc1945c2398dd
rimantascode <r.preiks@gmail.com>
2021-08-28 09:53:23 +0000

    edit is working

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.product_detail, name = "home" ),
     path('create/', views.add_product, name = "add" ),
+    path('edit/<int:product_id>', views.edit_product, name = "edit_product"),
    
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponse, redirect, reverse
 from .models import Product
 from .forms import ProductForm
 
@@ -18,3 +18,21 @@ def add_product(request):
     'form': form,
     }     
     return render(request, 'products/product_create.html', context)
+
+def edit_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    form = ProductForm(instance=product)
+    if request.method == "POST" :
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            form = ProductForm()
+            return HttpResponse("The product was updated succesfuly")
+            return redirect(reverse("home"))
+        else:
+            HttpResponse("<h1>The form is not correct</h1>")
+    context = {
+        "product": product,
+        "form": form   
+    }
+    return render(request, "products/edit_product.html", context)
