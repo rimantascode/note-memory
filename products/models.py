fc57a7b71756a77a9d0f4a392598b404eb6175a0
rimantascode <r.preiks@gmail.com>
2021-08-31 15:39:18 +0000

    fixed verose_name_plural and added friendly name

--- products/models.py
+++ products/models.py
@@ -5,5 +5,17 @@ class Product(models.Model):
     title = models.CharField(max_length=100, blank=False)
     Description = models.TextField(max_length=1000, blank=False)
     quantity = models.IntegerField(default=False)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.title
+
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+    
 
 
